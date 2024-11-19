0b0a32041b945215adece9912c44f103097904b8
Fajarramadhann1 <m.fajarramadhannn@gmail.com>
2024-11-19 14:13:35 +0800

    menambah pakan

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Models\Ayam;
+use App\Models\Post;
 use App\Models\Penjualan;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AyamController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\PenjualanController;
 
 Route::get('/', function () {
@@ -32,3 +34,12 @@ Route::post('/create-penjualan', [PenjualanController::class,'createPenjualan'])
 Route::get('/edit-penjualan/{penjualan}',[PenjualanController::class, 'showEditScreen']);
 Route::put('/edit-penjualan/{penjualan}', [PenjualanController::class, 'actuallyUpdatePenjualan']);
 Route::delete('/delete-penjualan/{penjualan}', [PenjualanController::class, 'deletePenjualan']);
+
+Route::get('/pakan', function () {
+    $posts = Post::all(); // Menampilkan semua artikel yang telah
+    return view('pakan', ['posts' => $posts]); // Pastikan 'about' adalah nama view yang benar
+});
+Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
