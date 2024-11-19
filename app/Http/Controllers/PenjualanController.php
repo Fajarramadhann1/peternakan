023af7cac725e33de661a2923c49dc745d857733
Fajarramadhann1 <m.fajarramadhannn@gmail.com>
2024-11-19 13:46:02 +0800

    menambah halaman penjualan

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Penjualan;
 use Illuminate\Http\Request;
 
 class PenjualanController extends Controller
 {
-    //
+    public function deletePenjualan(Penjualan $penjualan) {
+        //
+        $penjualan->delete();
+        return redirect('/penjualan');
+    }
+
+    public function actuallyUpdatePenjualan(Penjualan $penjualan, Request $request) {
+        $incomingField = $request->validate([
+            'metode_penjualan' => 'required',
+            'jumlah_penjualan' => 'required',
+        ]);
+
+        $incomingField['metode_penjualan'] = strip_tags($incomingField['metode_penjualan']);
+        $incomingField['jumlah_penjualan'] = strip_tags($incomingField['jumlah_penjualan']);
+
+        $penjualan->update($incomingField);
+        return redirect('/penjualan');
+    }
+
+    public function showEditScreen(Penjualan $penjualan) {
+        dd($penjualan); // Menampilkan data yang diambil
+        return view('edit-penjualan', ['penjualan' => $penjualan]);
+    }
+    
+
+    public function createPenjualan(Request $request) {
+        $incomingField = $request->validate([
+            'metode_penjualan' => 'required',
+            'jumlah_penjualan' => 'required',
+        ]);
+
+        $incomingField['metode_penjualan'] = strip_tags($incomingField['metode_penjualan']);
+        $incomingField['jumlah_penjualan'] = strip_tags($incomingField['jumlah_penjualan']);
+
+        Penjualan::create($incomingField);
+        return redirect('/penjualan');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Models\Ayam;
+use App\Models\Penjualan;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AyamController;
+use App\Http\Controllers\PenjualanController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,3 +21,14 @@ Route::get('/edit-ayam/{ayam}',[AyamController::class, 'showEditScreen']);
 Route::put('/edit-ayam/{ayam}',[AyamController::class,'actuallyUpdateAyam']);
 Route::delete('/delete-ayam/{ayam}', [AyamController::class, 'deleteAyam']);
 
+//Penjualan
+Route::get('/penjualan', function () {
+    $penjualans = Penjualan::all();
+    return view('penjualan', ['penjualans' => $penjualans]); 
+});
+
+//PenjualController
+Route::post('/create-penjualan', [PenjualanController::class,'createPenjualan']);
+Route::get('/edit-penjualan/{penjualan}',[PenjualanController::class, 'showEditScreen']);
+Route::put('/edit-penjualan/{penjualan}', [PenjualanController::class, 'actuallyUpdatePenjualan']);
+Route::delete('/delete-penjualan/{penjualan}', [PenjualanController::class, 'deletePenjualan']);
