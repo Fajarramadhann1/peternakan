c5b67b05a86ed06d649875f5f00a55e863453645
AlfinMHSPOLITALA <alfin23@mhs.politala.ac.id>
2024-12-14 16:21:02 +0800

    menambah route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Ayam;
-use App\Models\Post;
 use App\Models\Pakan;
 use App\Models\Penjualan;
 use Illuminate\Support\Facades\Route;
@@ -10,15 +9,13 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PakanController;
 use App\Http\Controllers\PenjualanController;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/ayam', function () {
     $ayams = Ayam::all();
-    return view('ayam', ['ayams' => $ayams]); 
+    return view('ayam', ['ayams' => $ayams]);
 });
 //AyamController
 Route::post('/create-ayam', [AyamController::class,'createAyam']);
@@ -26,27 +23,32 @@ Route::get('/edit-ayam/{ayam}',[AyamController::class, 'showEditScreen']);
 Route::put('/edit-ayam/{ayam}',[AyamController::class,'actuallyUpdateAyam']);
 Route::delete('/delete-ayam/{ayam}', [AyamController::class, 'deleteAyam']);
 
-
 //Penjualan
 Route::get('/penjualan', function () {
     $penjualans = Penjualan::all();
-    return view('penjualan', ['penjualans' => $penjualans]);});
-//PenjualController
+    return view('penjualan', ['penjualans' => $penjualans]);
+});
+//PenjualanController
 Route::post('/create-penjualan', [PenjualanController::class,'createPenjualan']);
 Route::get('/edit-penjualan/{penjualan}',[PenjualanController::class, 'showEditScreen']);
 Route::put('/edit-penjualan/{penjualan}', [PenjualanController::class, 'actuallyUpdatePenjualan']);
 Route::delete('/delete-penjualan/{penjualan}', [PenjualanController::class, 'deletePenjualan']);
 
-
-//pakan
+// Pakan
 Route::get('/pakan', function () {
     $pakans = Pakan::all();
-    return view('pakan', ['pakans' => $pakans]);});
-Route::post('/create-pakan', [PakanController::class, 'createPakan']);
+    return view('pakan', ['pakan' => $pakans]);
+});
+Route::post('/pakanbaru', [PakanController::class, 'pakanbaru']);
 Route::get('/edit-pakan/{pakan}', [PakanController::class, 'showEditScreen']);
 Route::put('/edit-pakan/{pakan}', [PakanController::class, 'actuallyUpdatePakan']);
 Route::delete('/delete-pakan/{pakan}', [PakanController::class, 'deletePakan']);
 
+// Tambahkan route untuk halaman pakanbaru
+Route::get('/pakanbaru', function () {
+    $pakans = Pakan::all(); // Mengambil semua data pakan
+    return view('pakanbaru', ['pakans' => $pakans]); // Mengirimkan data pakan ke view
+});
 
 Route::get('/login', function () {
     return view('login');
